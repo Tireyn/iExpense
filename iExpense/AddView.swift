d16044e45e88740d6b5376526f27961388477fdc
Sam McCullough <samanuel.mc@gmail.com>
2019-12-09 21:19:23 -0800

    Build out iExpense ContentView along with Expense and AddView

---
+++ iExpense/AddView.swift
@@ -0,0 +1,69 @@
+//
+//  AddView.swift
+//  iExpense
+//
+//  Created by Sam on 2019-12-08.
+//  Copyright © 2019 Sailfish Studios. All rights reserved.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    @Environment(\.presentationMode) var presentationMode
+    
+    @ObservedObject var expenses: Expenses
+    
+    @State private var name = ""
+    @State private var type = "Personal"
+    @State private var amount = ""
+    
+    @State private var errorTitle = ""
+    @State private var errorMessage = ""
+    @State private var showingError = false
+    
+    static let types = ["Business", "Personal"]
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                TextField("Name", text: $name)
+                
+                Picker("Type", selection: $type) {
+                    ForEach(Self.types, id: \.self) {
+                        Text($0)
+                    }
+                }
+                
+                TextField("Amount", text: $amount)
+                    .keyboardType(.numberPad)
+            }
+            .navigationBarTitle("Add new expense")
+            .navigationBarItems(trailing:
+                Button("Save") {
+                    if let actualAmount = Double(self.amount) {
+                        let item = ExpenseItem(name: self.name, type: self.type, amount: actualAmount)
+                        self.expenses.items.append(item)
+                        self.presentationMode.wrappedValue.dismiss()
+                    } else {
+                        self.error(title: "Invalid Value", message: "Please enter an Integer")
+                    }
+                }
+            )
+        }
+        .alert(isPresented: $showingError) {
+            Alert(title: Text(errorTitle), message: Text(errorMessage), dismissButton: .default(Text("OK")))
+        }
+    }
+    
+    func error(title: String, message: String) {
+        errorTitle = title
+        errorMessage = message
+        showingError = true
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddView(expenses: Expenses())
+    }
+}
--- iExpense/ContentView.swift
+++ iExpense/ContentView.swift
@@ -9,8 +9,43 @@
 import SwiftUI
 
 struct ContentView: View {
+    @ObservedObject var expenses = Expenses()
+    
+    @State private var showingAddExpense = false
+    
     var body: some View {
-        Text("Hello, World!")
+        NavigationView {
+            List {
+                ForEach(expenses.items) { item in
+                    HStack {
+                        VStack(alignment: .leading) {
+                            Text(item.name)
+                                .font(.headline)
+                            Text(item.type)
+                        }
+                        Spacer()
+                        Text("$\(item.amount, specifier: "%.2f")")
+                    }
+                }
+                .onDelete(perform: removeItems)
+            }
+            .navigationBarTitle("iExpense")
+            .navigationBarItems(leading:
+                Button(action: {
+                    self.showingAddExpense = true
+                }) {
+                    Image(systemName: "plus")
+                },
+                                trailing: EditButton()
+            )
+        }
+        .sheet(isPresented: $showingAddExpense) {
+            AddView(expenses: self.expenses)
+        }
+    }
+    
+    func removeItems(at offsets: IndexSet) {
+        expenses.items.remove(atOffsets: offsets)
     }
 }
 
